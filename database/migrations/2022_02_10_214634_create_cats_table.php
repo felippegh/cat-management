ed890d6a83cd8372a21b70818b139dc253e262ad
Felippe <felippegeorge@utexas.edu>
2022-02-12 17:48:56 -0300

    Create cats table migration

---
+++ database/migrations/2022_02_10_214634_create_cats_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('cats', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('breed');
+            $table->longText('description');
+            $table->double('location_lat');
+            $table->double('location_lon');
+            $table->date('birthdate');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('cats');
+    }
+};
