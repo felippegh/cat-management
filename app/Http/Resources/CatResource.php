8788ef37f7f0c59e316e32d20f820505a22751f1
Felippe <felippegeorge@utexas.edu>
2022-02-12 17:48:30 -0300

    Create cat resource

---
+++ app/Http/Resources/CatResource.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class CatResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
+     */
+    public function toArray($request)
+    {
+        return [
+            'name' => $this->resource->name,
+            'breed' => $this->resource->breed,
+            'description' => $this->resource->description,
+            'location_lat' => $this->resource->location_lat,
+            'location_lon' => $this->resource->location_lon,
+            'birthdate' => $this->resource->birthdate,
+        ];
+    }
+}
