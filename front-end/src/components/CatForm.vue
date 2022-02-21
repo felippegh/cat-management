dcfc7df680a02fc1094f49e43b7111ccb55a0a48
Felippe <felippegeorge@utexas.edu>
2022-02-21 18:59:05 -0300

    Create CatForm

---
+++ front-end/src/components/CatForm.vue
@@ -0,0 +1,28 @@
+<template>
+  <b-modal id="my-modal">Hello From My Modal! This is cat {{cat.id}}</b-modal>
+</template>
+
+<script>
+
+import Cats from "../api/endpoints/Cats";
+
+export default {
+  name: 'CatForm',
+
+  data() {
+    return {
+      cat: {}
+    };
+  },
+
+  methods: {
+  },
+
+  created() {
+  }
+}
+</script>
+
+<style lang="scss">
+
+</style>
