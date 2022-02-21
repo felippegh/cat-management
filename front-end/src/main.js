5eae5c160746d09110449548a9c6ea21fe0a75fe
Felippe <felippegeorge@utexas.edu>
2022-02-21 18:59:33 -0300

    Add imports to main

--- front-end/src/main.js
+++ front-end/src/main.js
@@ -1,8 +1,29 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from 'vue';
+import App from './App.vue';
 
-Vue.config.productionTip = false
+import {
+  Menu,
+  VTooltip,
+} from 'floating-vue';
+import 'floating-vue/dist/style.css';
+
+import VueSweetalert2 from 'vue-sweetalert2';
+import 'sweetalert2/dist/sweetalert2.min.css';
+
+import { BootstrapVue   } from 'bootstrap-vue';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap-vue/dist/bootstrap-vue.css';
+
+
+Vue.use(VueSweetalert2);
+
+Vue.use(BootstrapVue);
+
+Vue.component('VMenu', Menu);
+Vue.directive('tooltip', VTooltip);
+
+Vue.config.productionTip = false;
 
 new Vue({
   render: h => h(App),
-}).$mount('#app')
+}).$mount('#app');
