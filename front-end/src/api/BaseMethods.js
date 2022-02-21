ff8ddd06ab30fd178ed2d9f8545d1dd058b079ed
Felippe <felippegeorge@utexas.edu>
2022-02-21 18:57:37 -0300

    change baseUrl to route

--- front-end/src/api/BaseMethods.js
+++ front-end/src/api/BaseMethods.js
@@ -2,30 +2,30 @@ import getData from "./utils/getData";
 import BaseConfigs from "./BaseConfigs";
 
 export default class BaseMethods extends BaseConfigs {
-    baseUrl;
+    route;
 
     async index(params) {
-        return this.api.get(this.baseUrl, { params }).then(getData);
+        return this.api.get(this.route, { params }).then(getData);
     }
 
     async store(data) {
-        return this.api.post(this.baseUrl, data).then(getData);
+        return this.api.post(this.route, data).then(getData);
     }
 
     async show(id, params) {
-        const url = this.baseUrl + "/" + id;
+        const url = this.route + "/" + id;
 
         return this.api.get(url, { params }).then(getData);
     }
 
     async update(id, data) {
-        const url = this.baseUrl + "/" + id;
+        const url = this.route + "/" + id;
 
         return this.api.put(url, data).then(getData);
     }
 
     async destroy(id) {
-        const url = this.baseUrl + "/" + id;
+        const url = this.route + "/" + id;
 
         return this.api.delete(url).then(getData);
     }
