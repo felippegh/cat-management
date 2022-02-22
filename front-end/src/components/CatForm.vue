775e1e98d201b929e4896047317d9b6b96c46ef2
Felippe <felippegeorge@utexas.edu>
2022-02-22 18:04:35 -0300

    Add properties, form and submitform method

--- front-end/src/components/CatForm.vue
+++ front-end/src/components/CatForm.vue
@@ -1,5 +1,14 @@
 <template>
-  <b-modal id="my-modal">Hello From My Modal! This is cat {{cat.id}}</b-modal>
+  <b-modal id="my-modal" title="Edit Cat" ok-title="Save" @ok="submitForm">
+    <label for="cat-name">Cat Name:</label>
+    <b-form-input id="cat-name" v-model="cat.name" placeholder="Enter the cat name"></b-form-input>
+    <br>
+    <label for="cat-breed">Cat Breed:</label>
+    <b-form-input id="cat-breed" v-model="cat.breed" placeholder="Enter the cat breed"></b-form-input>
+    <br>
+    <label for="cat-birth">Cat Birthdate:</label>
+    <b-form-datepicker id="cat-birth" v-model="cat.birthdate" placeholder="Enter the birthdate"></b-form-datepicker>
+  </b-modal>
 </template>
 
 <script>
@@ -9,17 +18,50 @@ import Cats from "../api/endpoints/Cats";
 export default {
   name: 'CatForm',
 
+  props: {
+    catEdit: {
+      type: Object,
+    },
+  },
+
   data() {
     return {
-      cat: {}
+      cat: {
+        id : null,
+        name: null,
+        breed: null,
+        birthdate: null,
+        location_lat: null,
+        location_lon: null,
+      }
     };
   },
 
-  methods: {
+  watch: {
+    catEdit(){
+      this.defineCat();
+    },
   },
 
-  created() {
-  }
+  methods: {
+    defineCat() {
+      if(this.catEdit){
+        this.cat = this.catEdit;
+      }
+    },
+    submitForm() {
+      if(this.cat.id){
+        return this.updateCat();
+      }
+      return this.storeCat();
+    },
+    storeCat() {
+      Cats.store(this.cat).then()
+    },
+    updateCat() {
+      Cats.update(this.cat.id, this.cat);
+    }
+  },
 }
 </script>
 
